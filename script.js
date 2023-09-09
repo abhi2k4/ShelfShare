89358cae9a2cc63bed75462d6346e56e733b7598
Abhishek Thrormothe <abhishekmt2004@gmail.com>
2023-09-09 12:39:26 +0530

    hii

--- script.js
+++ script.js
@@ -7,11 +7,10 @@ bookForm.addEventListener('submit', function (event) {
   const author = document.getElementById('author').value;
   const price = document.getElementById('price').value;
 
-  // Here you can send this data to the server using AJAX or fetch API
-  // to store it in the database.
   
   console.log('Book added:', title, author, price);
-  
-  // Clear form inputs
+
   bookForm.reset();
 });
+
+
