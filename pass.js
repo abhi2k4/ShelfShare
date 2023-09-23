2189383d837bd577839d41eb536fb3307e3c6a8a
Abhishek Thrormothe <abhishekmt2004@gmail.com>
2023-09-24 00:32:27 +0530

    p

--- pass.js
+++ pass.js
@@ -28,26 +28,26 @@ function validatePassword(password) {
     const hasSpecialChars = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(password);
 
     if (password.length < minLength) {
-        return '> Password must be at least ' + minLength + ' characters long. <';
+        return ' Password must be at least ' + minLength + ' characters long. ';
     }
 
     if (!hasUpperCase) {
-        return '> Password must contain at least one uppercase letter. <';
+        return ' Password must contain at least one uppercase letter. ';
     }
 
     if (!hasLowerCase) {
-        return '> Password must contain at least one lowercase letter. <';
+        return ' Password must contain at least one lowercase letter. ';
     }
 
     if (!hasNumbers) {
-        return '> Password must contain at least one number. <';
+        return ' Password must contain at least one number. ';
     }
 
     if (!hasSpecialChars) {
-        return '> Password must contain at least one special character. <';
+        return ' Password must contain at least one special character. ';
     }
 
-    return '> Password is valid. <';
+    return ' Password is valid. ';
 }
 
 const passwordInput = document.getElementById('password');
