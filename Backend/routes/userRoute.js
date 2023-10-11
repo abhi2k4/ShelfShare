525c69ae10ead8d2c69a09c1952865c6d1df7698
Abhishek Thrormothe <abhishekmt2004@gmail.com>
2023-10-11 23:58:56 +0530

    h

--- Backend/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authController = require("../controller/authController");
-
-const router = express.Router();
-
-router.post("/signup", authController.signup);
-router.post("/login", authController.login);
-
-module.exports = router;
